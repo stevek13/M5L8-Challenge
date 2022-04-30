960ea77228282559290de0f3a2d1c307d5957dec
Steve Kite <skite1@hotmail.com>
2022-04-30 10:59:39 -0700

    Initial Commit

---
+++ M5L8 Challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  M5L8 Challenge
+//
+//  Created by Steve Kite on 4/30/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ M5L8 Challenge/M5L8_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  M5L8_ChallengeApp.swift
+//  M5L8 Challenge
+//
+//  Created by Steve Kite on 4/30/22.
+//
+
+import SwiftUI
+
+@main
+struct M5L8_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
